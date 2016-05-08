7c7becfcee8429cea2b813a45bc55834a21159bb
Kamran Amini <kam.cpp@gmail.com>
2016-05-08 10:55:55 +0430

    Fence slave pop tool has been refactored.

--- slave/pop/main.cc
+++ slave/pop/main.cc
@@ -2,23 +2,23 @@
 #include <chrono>
 #include <iostream>
 
-#include <naeem/hottentot/runtime/configuration.h>
-#include <naeem/hottentot/runtime/logger.h>
-#include <naeem/hottentot/runtime/utils.h>
-#include <naeem/hottentot/runtime/proxy/proxy.h>
-#include <naeem/hottentot/runtime/proxy/proxy_runtime.h>
+#include <org/labcrypto/hottentot/runtime/configuration.h>
+#include <org/labcrypto/hottentot/runtime/logger.h>
+#include <org/labcrypto/hottentot/runtime/utils.h>
+#include <org/labcrypto/hottentot/runtime/proxy/proxy.h>
+#include <org/labcrypto/hottentot/runtime/proxy/proxy_runtime.h>
 
-#include <gate/proxy/gate_service.h>
-#include <gate/proxy/gate_service_proxy_builder.h>
+#include <fence/proxy/fence_service.h>
+#include <fence/proxy/fence_service_proxy_builder.h>
 
 
 void PrintHelpMessage() {
   std::cout << "Usage: " << std::endl;
-  std::cout << "  ./naeem-gate-slave-pop [ARGUMENTS]" << std::endl;
+  std::cout << "  ./naeem-fence-slave-pop [ARGUMENTS]" << std::endl;
   std::cout << std::endl;
   std::cout << "  ARGUMENTS:" << std::endl;
-  std::cout << "        -h | --host                Slave gate host address [Mandatory]" << std::endl;
-  std::cout << "        -p | --port                Slave gate port [Mandatory]" << std::endl;
+  std::cout << "        -h | --host                Slave fence host address [Mandatory]" << std::endl;
+  std::cout << "        -p | --port                Slave fence port [Mandatory]" << std::endl;
   std::cout << "        -l | --label               Pop from queue with this label [Mandatory]" << std::endl;
   std::cout << "        -v                         Verbose mode [Optional]" << std::endl;
 }
@@ -26,108 +26,108 @@ void PrintHelpMessage() {
 int 
 main(int argc, char **argv) {
   try {
-    ::naeem::hottentot::runtime::Logger::Init();
-    ::naeem::hottentot::runtime::Logger::GetOut() << "NTNAEEM CO." << std::endl;
-    ::naeem::hottentot::runtime::Logger::GetOut() << "COPYRIGHT 2015-2016" << std::endl;
-    ::naeem::hottentot::runtime::Logger::GetOut() << "NAEEM GATE SLAVE POP CLIENT" << std::endl;
-    ::naeem::hottentot::runtime::Logger::GetOut() << std::endl;
-    ::naeem::hottentot::runtime::Configuration::Init(argc, argv);
-    ::naeem::hottentot::runtime::proxy::ProxyRuntime::Init(argc, argv);
-    if (!::naeem::hottentot::runtime::Configuration::Exists("h", "host")) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "ERROR: Slave gate host is not specified." << std::endl;
+    ::org::labcrypto::hottentot::runtime::Logger::Init();
+    ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "NTNAEEM CO." << std::endl;
+    ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "COPYRIGHT 2015-2016" << std::endl;
+    ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "NAEEM GATE SLAVE POP CLIENT" << std::endl;
+    ::org::labcrypto::hottentot::runtime::Logger::GetOut() << std::endl;
+    ::org::labcrypto::hottentot::runtime::Configuration::Init(argc, argv);
+    ::org::labcrypto::hottentot::runtime::proxy::ProxyRuntime::Init(argc, argv);
+    if (!::org::labcrypto::hottentot::runtime::Configuration::Exists("h", "host")) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "ERROR: Slave fence host is not specified." << std::endl;
       PrintHelpMessage();
       exit(1);
     }
-    if (!::naeem::hottentot::runtime::Configuration::HasValue("h", "host")) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "ERROR: Slave gate host is not specified." << std::endl;
+    if (!::org::labcrypto::hottentot::runtime::Configuration::HasValue("h", "host")) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "ERROR: Slave fence host is not specified." << std::endl;
       PrintHelpMessage();
       exit(1);
     }
-    if (!::naeem::hottentot::runtime::Configuration::Exists("p", "port")) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "ERROR: Slave gate port is not specified." << std::endl;
+    if (!::org::labcrypto::hottentot::runtime::Configuration::Exists("p", "port")) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "ERROR: Slave fence port is not specified." << std::endl;
       PrintHelpMessage();
       exit(1);
     }
-    if (!::naeem::hottentot::runtime::Configuration::HasValue("p", "port")) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "ERROR: Slave gate port is not specified." << std::endl;
+    if (!::org::labcrypto::hottentot::runtime::Configuration::HasValue("p", "port")) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "ERROR: Slave fence port is not specified." << std::endl;
       PrintHelpMessage();
       exit(1);
     }
-    if (!::naeem::hottentot::runtime::Configuration::Exists("l", "label")) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "ERROR: Message label is not specified." << std::endl;
+    if (!::org::labcrypto::hottentot::runtime::Configuration::Exists("l", "label")) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "ERROR: Message label is not specified." << std::endl;
       PrintHelpMessage();
       exit(1);
     }
-    if (!::naeem::hottentot::runtime::Configuration::HasValue("l", "label")) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "ERROR: Message label is not specified." << std::endl;
+    if (!::org::labcrypto::hottentot::runtime::Configuration::HasValue("l", "label")) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "ERROR: Message label is not specified." << std::endl;
       PrintHelpMessage();
       exit(1);
     }
-    std::string host = ::naeem::hottentot::runtime::Configuration::AsString("h", "host");
-    uint16_t port = ::naeem::hottentot::runtime::Configuration::AsUInt32("p", "port");
-    std::string label = ::naeem::hottentot::runtime::Configuration::AsString("l", "label");
-    if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-      ::naeem::hottentot::runtime::Logger::GetOut() << "Proxy runtime is initialized." << std::endl;
+    std::string host = ::org::labcrypto::hottentot::runtime::Configuration::AsString("h", "host");
+    uint16_t port = ::org::labcrypto::hottentot::runtime::Configuration::AsUInt32("p", "port");
+    std::string label = ::org::labcrypto::hottentot::runtime::Configuration::AsString("l", "label");
+    if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Proxy runtime is initialized." << std::endl;
     }
-    ::ir::ntnaeem::gate::proxy::GateService *proxy = 
-      ::ir::ntnaeem::gate::proxy::GateServiceProxyBuilder::Create(host, port);
-    if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-      ::naeem::hottentot::runtime::Logger::GetOut() << "Proxy object is created." << std::endl;
-      ::naeem::hottentot::runtime::Logger::GetOut() << "Target is " << host << ":" << port << std::endl;
+    ::org::labcrypto::fence::proxy::FenceService *proxy = 
+      ::org::labcrypto::fence::proxy::FenceServiceProxyBuilder::Create(host, port);
+    if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Proxy object is created." << std::endl;
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Target is " << host << ":" << port << std::endl;
     }
     try {
       //=============================================
-      if (dynamic_cast< ::naeem::hottentot::runtime::proxy::Proxy*>(proxy)->IsServerAlive()) {
-        ::naeem::hottentot::runtime::types::Boolean hasMore;
-        ::naeem::hottentot::runtime::types::Utf8String labelString(label);
+      if (dynamic_cast< ::org::labcrypto::hottentot::runtime::proxy::Proxy*>(proxy)->IsServerAlive()) {
+        ::org::labcrypto::hottentot::Boolean hasMore;
+        ::org::labcrypto::hottentot::Utf8String labelString(label);
         proxy->HasMore(labelString, hasMore);
         if (!hasMore.GetValue()) {
-          ::naeem::hottentot::runtime::Logger::GetOut() << "NO MESSAGE IS FOUND WITH THIS LABEL." << std::endl;
+          ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "NO MESSAGE IS FOUND WITH THIS LABEL." << std::endl;
         } else {
-          ::ir::ntnaeem::gate::Message message;
+          ::org::labcrypto::fence::Message message;
           proxy->PopNext(labelString, message);
-          ::naeem::hottentot::runtime::Logger::GetOut() << 
+          ::org::labcrypto::hottentot::runtime::Logger::GetOut() << 
             "Message is popped with id: " << message.GetId().GetValue() << std::endl;
-          ::naeem::hottentot::runtime::Logger::GetOut() << 
+          ::org::labcrypto::hottentot::runtime::Logger::GetOut() << 
             "Id: " << message.GetId().GetValue() << std::endl;
-          ::naeem::hottentot::runtime::Logger::GetOut() << 
+          ::org::labcrypto::hottentot::runtime::Logger::GetOut() << 
             "Related Id: " << message.GetRelId().GetValue() << std::endl;
-          ::naeem::hottentot::runtime::Logger::GetOut() << 
+          ::org::labcrypto::hottentot::runtime::Logger::GetOut() << 
             "Label: " << message.GetLabel().ToStdString() << std::endl;
-          ::naeem::hottentot::runtime::Logger::GetOut() << 
+          ::org::labcrypto::hottentot::runtime::Logger::GetOut() << 
             "Content: " << std::endl << message.GetContent() << std::endl;
         }
       } else {
-        ::naeem::hottentot::runtime::Logger::GetOut() << "ERROR: Gate is not available." << std::endl;
+        ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "ERROR: Fence is not available." << std::endl;
       }
       //=============================================
-      ::ir::ntnaeem::gate::proxy::GateServiceProxyBuilder::Destroy(proxy);
-      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-        ::naeem::hottentot::runtime::Logger::GetOut() << "Proxy object is destroyed." << std::endl;
+      ::org::labcrypto::fence::proxy::FenceServiceProxyBuilder::Destroy(proxy);
+      if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+        ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Proxy object is destroyed." << std::endl;
       }
-      ::naeem::hottentot::runtime::proxy::ProxyRuntime::Shutdown();
-      ::naeem::hottentot::runtime::Logger::Shutdown();
+      ::org::labcrypto::hottentot::runtime::proxy::ProxyRuntime::Shutdown();
+      ::org::labcrypto::hottentot::runtime::Logger::Shutdown();
     } catch (std::exception &e) {
-      ::naeem::hottentot::runtime::Logger::GetError() << e.what() << std::endl;
-      ::ir::ntnaeem::gate::proxy::GateServiceProxyBuilder::Destroy(proxy);
-      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-        ::naeem::hottentot::runtime::Logger::GetOut() << "Proxy object is destroyed." << std::endl;
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << e.what() << std::endl;
+      ::org::labcrypto::fence::proxy::FenceServiceProxyBuilder::Destroy(proxy);
+      if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+        ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Proxy object is destroyed." << std::endl;
       }
-      ::naeem::hottentot::runtime::proxy::ProxyRuntime::Shutdown();
-      ::naeem::hottentot::runtime::Logger::Shutdown();
+      ::org::labcrypto::hottentot::runtime::proxy::ProxyRuntime::Shutdown();
+      ::org::labcrypto::hottentot::runtime::Logger::Shutdown();
       exit(1);
     } catch (...) {
-      ::naeem::hottentot::runtime::Logger::GetError() << "Error." << std::endl;
-      ::ir::ntnaeem::gate::proxy::GateServiceProxyBuilder::Destroy(proxy);
-      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-        ::naeem::hottentot::runtime::Logger::GetOut() << "Proxy object is destroyed." << std::endl;
+      ::org::labcrypto::hottentot::runtime::Logger::GetError() << "Error." << std::endl;
+      ::org::labcrypto::fence::proxy::FenceServiceProxyBuilder::Destroy(proxy);
+      if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+        ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Proxy object is destroyed." << std::endl;
       }
-      ::naeem::hottentot::runtime::proxy::ProxyRuntime::Shutdown();
-      ::naeem::hottentot::runtime::Logger::Shutdown();
+      ::org::labcrypto::hottentot::runtime::proxy::ProxyRuntime::Shutdown();
+      ::org::labcrypto::hottentot::runtime::Logger::Shutdown();
       exit(1);
     }
   } catch (...) {
-    ::naeem::hottentot::runtime::Logger::GetError() << "Error." << std::endl;
+    ::org::labcrypto::hottentot::runtime::Logger::GetError() << "Error." << std::endl;
     exit(1);
   }
   return 0;
